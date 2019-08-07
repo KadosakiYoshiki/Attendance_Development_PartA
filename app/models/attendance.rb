da40ec978d3f5b5371cbe9349097ee824dca9457
EC2 Default User <ec2-user@ip-172-31-40-109.us-east-2.compute.internal>
2019-08-07 18:08:21 +0000

    完成型４

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,7 +1,6 @@
 class Attendance < ApplicationRecord
   belongs_to :user
-  has_one :attendancelogs, dependent: :destroy
-  
+
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
   
